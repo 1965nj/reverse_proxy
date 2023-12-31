8cad4b18ab2359d0766698052e3f475a67047f55
1965nj <jafaroun1998@gmail.com>
2023-12-31 11:25:45 +0100

    use this file to starting project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 
 import './App.css';
-import Navbar from './components/navbar';
-import Header from './components/Header';
+
+
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        <Navbar />
-        
-        <Header />
-        
+
       </header>
     </div>
   );
